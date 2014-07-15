de47a060961a1d22afe05fcdb13cd876ba151dfd
Christophe <christophe@cs.ucsb.edu>
2014-07-14 19:32:19 -0700

    cle_bdf now also get number of bits per address from libbfd

--- bfd/cle_bfd.c
+++ bfd/cle_bfd.c
@@ -3,8 +3,69 @@
 #include <elf.h>
 #include <stdio.h>
 
+/* These are a bunch of simple Ctypes friendly functions using the BFD library
+ * to recover information that is needed by Clé */
 
-const char *get_bfd_arch(const char* filename)
+
+static inline const bfd_arch_info_type *get_bfd_info(const char *filename);
+static bfd *open_bfd(const char* filename);
+
+/* This gives the name of the architecture, e.g.: mips*/
+const char *get_bfd_arch_name(const char* filename)
+{
+
+    const bfd_arch_info_type *info;
+
+    info = get_bfd_info(filename);
+    return info->arch_name;
+}
+
+
+/* This gives the printable name of the architecture, e.g.: mips:3000*/
+const char *get_bfd_arch_pname(const char* filename)
+{
+    bfd *bfd;
+
+    bfd = open_bfd(filename);
+    if (!bfd)
+        return NULL;
+
+
+    return bfd_printable_name(bfd);
+}
+
+
+/* How many bits per address on this architecture ? */
+int get_bits_per_addr(const char *filename)
+{
+
+    const bfd_arch_info_type *info;
+
+    info = get_bfd_info(filename);
+    return info->bits_per_address;
+}
+
+
+int get_arch_size(const char *filename)
+{
+    int arch_size;
+
+    bfd *bfd;
+    bfd = open_bfd(filename);
+    if (!bfd)
+        return 0;
+
+    arch_size = bfd_get_arch_size (bfd);
+
+    return arch_size;
+}
+
+
+
+/* ##### The following static functions are not meant to be called from Python
+ * ####*/
+
+static bfd *open_bfd(const char* filename)
 {
     bfd *bfd;
     bfd_boolean r;
@@ -15,21 +76,25 @@ const char *get_bfd_arch(const char* filename)
     bfd_init();
     bfd = bfd_openr(filename, NULL);
 
-    if (!bfd)
-    {
-        printf("Error, NULL bfd\n");
-        return NULL;
-    }
-
     r = bfd_check_format(bfd, bfd_object);
-
-    if (!r) {
-        printf("WFT??\n");
+    if (!r)
         return NULL;
-    }
 
-    //arch_size = bfd_get_arch_size (bfd);
-
-    return bfd_printable_name(bfd);
+    return bfd;
 }
+
+
+
+
+/* Stub to recover the bfd_arch_info_type structure */
+static inline const bfd_arch_info_type *get_bfd_info(const char *filename)
+{
+    const char *archname;
+    const bfd_arch_info_type *info;
+
+    archname = get_bfd_arch_pname(filename);
+    info = bfd_scan_arch(archname);
+    return info;
+}
+
 
