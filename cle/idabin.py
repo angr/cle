ac47018ba73e2e0e561d982b9288fcb4ea68f11c
Christophe <christophe@cs.ucsb.edu>
2014-10-09 17:00:41 -0700

    Avoid raising exceptions in exceptions :)

--- cle/idabin.py
+++ cle/idabin.py
@@ -85,7 +85,7 @@ class IdaBin(AbsObj):
 
         # If we reach this point, we should have the addresses
         if self.got_begin is None or self.got_end is None:
-            raise CLException("This architecture has no section %s :(", sec_name)
+            raise CLException("This architecture has no section %s :(" % sec_name)
 
     def __in_proper_section(self, addr):
         """ Is @addr in the proper section for this architecture ?"""
