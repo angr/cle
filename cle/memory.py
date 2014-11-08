ab7e56a75582b468e5ccfae3f89090b52ab1a40d
Christophe <christophe@cs.ucsb.edu>
2014-11-07 16:02:05 -0800

    Clemory class for general operations on memory.

---
+++ cle/memory.py
@@ -0,0 +1,9 @@
+class Clemory(dict):
+
+    def read_bytes(self, addr, n):
+        """ Read @n bytes at address @addr in memory and return an array of bytes
+        """
+        b = []
+        for i in range(addr, addr+n):
+            b.append(self.get(i))
+        return b
