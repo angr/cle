3471048293d5911d9b651b90db98eae261a12ee6
Yan <yans@yancomm.net>
2015-05-05 18:57:45 -0700

    include non-git dependencies

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from distutils.core import setup
 
-setup(name='cle', version='1.00', packages=['cle'])
+setup(
+    name='cle',
+    version='1.00',
+    packages=['cle'],
+    install_requires=[i.strip() for i in open('requirements.txt').readlines() if 'git' not in i]
+)
