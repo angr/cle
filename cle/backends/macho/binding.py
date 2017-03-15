d402a60c7b6c933f8ae29824b7d9ce09026c6a52
Andrew Dutcher <andrew@andrewdutcher.com>
2017-03-15 16:34:14 -0700

    Lint macho/binding.py

--- cle/backends/macho/binding.py
+++ cle/backends/macho/binding.py
@@ -2,9 +2,11 @@
 # This file is part of Mach-O Loader for CLE.
 # Contributed December 2016 by Fraunhofer SIT (https://www.sit.fraunhofer.de/en/).
 
-from ...errors import CLECompatibilityError, CLEInvalidBinaryError
-import logging, struct
+import struct
 
+from ...errors import CLEInvalidBinaryError
+
+import logging
 l = logging.getLogger('cle.MachO.binding')
 
 OPCODE_MASK = 0xF0
@@ -70,33 +72,32 @@ def read_sleb(blob, offset):
 class BindingState(object):
     """State object"""
 
-    def __init__(s, is_64):
-        s.index = 0
-        s.done = False
-        s.lib_ord = 0
-        s.sym_name = ""
-        s.sym_flags = 0
-        s.binding_type = 0
-        s.addend = 0
-        s.segment_index = 0
-        s.address = 0
-        s.seg_end_address = 0  # TODO: no rebasing support
-        s.wraparound = 2 ** 64  # address is expected to properly overflow and address is uintptr_t (unsigned long according to _uintptr_t.h)
-        s.sizeof_intptr_t = 8 if is_64 else 4  # experimentally determined
-        s.bind_handler = None  # function(state,binary) => None
+    def __init__(self, is_64):
+        self.index = 0
+        self.done = False
+        self.lib_ord = 0
+        self.sym_name = ""
+        self.sym_flags = 0
+        self.binding_type = 0
+        self.addend = 0
+        self.segment_index = 0
+        self.address = 0
+        self.seg_end_address = 0  # TODO: no rebasing support
+        self.wraparound = 2 ** 64  # address is expected to properly overflow and address is uintptr_t (unsigned long according to _uintptr_t.h)
+        self.sizeof_intptr_t = 8 if is_64 else 4  # experimentally determined
+        self.bind_handler = None  # function(state,binary) => None
 
-    def add_address_ov(s, address, addend):
+    def add_address_ov(self, address, addend):
         """ this is a very ugly klugde. It is needed because dyld relies on overflow
             semantics and represents several negative offsets through BIG ulebs"""
         tmp = address + addend
-        if tmp > s.wraparound:
-            tmp -= s.wraparound
-        s.address = tmp
+        if tmp > self.wraparound:
+            tmp -= self.wraparound
+        self.address = tmp
 
     def check_address_bounds(self):
         if self.address >= self.seg_end_address:
-            l.error(
-                "address >= seg_end_address (0x{1:X}>=0x{2:X})".format(self.index, self.address, self.seg_end_address))
+            l.error("index %d: address >= seg_end_address (%#x >= %#x)", self.index, self.address, self.seg_end_address)
             raise CLEInvalidBinaryError()
 
 
@@ -188,7 +189,7 @@ class BindingHelper(object):
         s.seg_end_address = seg.vaddr + seg.memsize  # TODO: no rebasing support
         end = len(blob)
         while not s.done and s.index < end:
-            l.debug("Current address: 0x{0:X}, blob index (offset): 0x{1:X}".format(s.address, s.index))
+            l.debug("Current address: %#x, blob index (offset): %#x", s.address, s.index)
             raw_opcode = struct.unpack("B", blob[s.index])[0]
             opcode = raw_opcode & OPCODE_MASK
             immediate = raw_opcode & IMM_MASK
@@ -197,21 +198,22 @@ class BindingHelper(object):
                 h = opcode_dict[opcode]
                 s = h(s, self.binary, immediate, blob)
             except KeyError:
-                l.error("Invalid opcode for current binding: 0x{0:X}".format(opcode))
+                l.error("Invalid opcode for current binding: %#x", opcode)
 
         return s
 
 
+# pylint: disable=unused-argument
 # The following functions realize different variants of handling binding opcodes
 # the format is def X(state,binary,immediate,blob) => state
 def n_opcode_done(s, b, i, blob):
-    l.debug("BIND_OPCODE_DONE @ 0x{0:X}".format(s.index))
+    l.debug("BIND_OPCODE_DONE @ %#x", s.index)
     s.done = True
     return s
 
 
 def n_opcode_set_dylib_ordinal_imm(s, b, i, blob):
-    l.debug("SET_DYLIB_ORDINAL_IMM @ 0x{0:X}: {1}".format(s.index, i))
+    l.debug("SET_DYLIB_ORDINAL_IMM @ %#x: %d", s.index, i)
     s.lib_ord = i
     return s
 
@@ -220,7 +222,7 @@ def n_opcode_set_dylib_ordinal_uleb(s, b, i, blob):
     uleb = read_uleb(blob, s.index)
     s.lib_ord = uleb[0]
     s.index += uleb[1]
-    l.debug("SET_DYLIB_ORDINAL_ULEB @ 0x{0:X}: {1}".format(s.index, s.lib_ord))
+    l.debug("SET_DYLIB_ORDINAL_ULEB @ %#x: %d", s.index, s.lib_ord)
     return s
 
 
@@ -229,7 +231,7 @@ def n_opcode_set_dylib_special_imm(s, b, i, blob):
         s.lib_ord = 0
     else:
         s.lib_ord = (i | OPCODE_MASK) - 256
-    l.debug("SET_DYLIB_SPECIAL_IMM @ 0x{0:X}: {1}".format(s.index, s.lib_ord))
+    l.debug("SET_DYLIB_SPECIAL_IMM @ %#x: %d", s.index, s.lib_ord)
     return s
 
 
@@ -240,21 +242,20 @@ def n_opcode_set_trailing_flags_imm(s, b, i, blob):
         s.sym_name += blob[s.index]
         s.index += 1
     s.index += 1  # move past 0 byte
-    l.debug("SET_SYMBOL_TRAILING_FLAGS_IMM @ 0x{0:X}: '{1}',0x{2:X}".format(s.index - len(s.sym_name) - 1, s.sym_name,
-                                                                            s.sym_flags))
+    l.debug("SET_SYMBOL_TRAILING_FLAGS_IMM @ %#x: %r,%#x", s.index - len(s.sym_name) - 1, s.sym_name, s.sym_flags)
     return s
 
 
 def n_opcode_set_type_imm(s, b, i, blob):
     s.binding_type = i
-    l.debug("SET_TYPE_IMM @ 0x{0:X}: {1}".format(s.index, s.binding_type))
+    l.debug("SET_TYPE_IMM @ %#x: %d", s.index, s.binding_type)
     return s
 
 
 def n_opcode_set_addend_sleb(s, b, i, blob):
     sleb = read_sleb(blob, s.index)
     s.addend = sleb[0]
-    l.debug("SET_ADDEND_SLEB @ 0x{0:X}: {1}".format(s.index, s.addend))
+    l.debug("SET_ADDEND_SLEB @ %#x: %d", s.index, s.addend)
     s.index += sleb[1]
     return s
 
@@ -262,7 +263,7 @@ def n_opcode_set_addend_sleb(s, b, i, blob):
 def n_opcode_set_segment_and_offset_uleb(s, b, i, blob):
     s.segment_index = i
     uleb = read_uleb(blob, s.index)
-    l.debug("(n)SET_SEGMENT_AND_OFFSET_ULEB @ 0x{0:X}: {1}, {2}".format(s.index, s.segment_index, uleb[0]))
+    l.debug("(n)SET_SEGMENT_AND_OFFSET_ULEB @ %#x: %d, %d", s.index, s.segment_index, uleb[0])
     s.index += uleb[1]
     seg = b.segments[s.segment_index]
     s.add_address_ov(seg.vaddr, uleb[0])
@@ -273,7 +274,7 @@ def n_opcode_set_segment_and_offset_uleb(s, b, i, blob):
 
 def l_opcode_set_segment_and_offset_uleb(s, b, i, blob):
     uleb = read_uleb(blob, s.index)
-    l.debug("(l)SET_SEGMENT_AND_OFFSET_ULEB @ 0x{0:X}: {1}, {2}".format(s.index, i, uleb[0]))
+    l.debug("(l)SET_SEGMENT_AND_OFFSET_ULEB @ %#x: %d, %d", s.index, i, uleb[0])
     seg = b.segments[i]
     s.add_address_ov(seg.vaddr, uleb[0])
     s.index += uleb[1]
@@ -283,13 +284,13 @@ def l_opcode_set_segment_and_offset_uleb(s, b, i, blob):
 def n_opcode_add_addr_uleb(s, b, i, blob):
     uleb = read_uleb(blob, s.index)
     s.add_address_ov(s.address, uleb[0])
-    l.debug("ADD_ADDR_ULEB @ 0x{0:X}: {1}".format(s.index, uleb[0]))
+    l.debug("ADD_ADDR_ULEB @ %#x: %d", s.index, uleb[0])
     s.index += uleb[1]
     return s
 
 
 def n_opcode_do_bind(s, b, i, blob):
-    l.debug("(n)DO_BIND @ 0x{0:X}".format(s.index))
+    l.debug("(n)DO_BIND @ %#x", s.index)
     s.check_address_bounds()
     s.bind_handler(s, b)
     s.add_address_ov(s.address, s.sizeof_intptr_t)
@@ -297,18 +298,17 @@ def n_opcode_do_bind(s, b, i, blob):
 
 
 def l_opcode_do_bind(s, b, i, blob):
-    l.debug("(l)DO_BIND @ 0x{0:X}".format(s.index))
+    l.debug("(l)DO_BIND @ %#x", s.index)
     s.bind_handler(s, b)
     return s
 
 
 def n_opcode_do_bind_add_addr_uleb(s, b, i, blob):
     uleb = read_uleb(blob, s.index)
-    l.debug("DO_BIND_ADD_ADDR_ULEB @ 0x{0:X}: {0}".format(s.index, uleb[0]))
+    l.debug("DO_BIND_ADD_ADDR_ULEB @ %#x: %d", s.index, uleb[0])
     if s.address >= s.seg_end_address:
-        l.error(
-            "DO_BIND_ADD_ADDR_ULEB @ 0x{0:X}: address >= seg_end_address (0x{1:X}>=0x{2:X})".format(s.index, s.address,
-                                                                                                    s.seg_end_address))
+        l.error("DO_BIND_ADD_ADDR_ULEB @ %#x: address >= seg_end_address (%#x>=%#x)",
+                s.index, s.address, s.seg_end_address)
         raise CLEInvalidBinaryError()
     s.index += uleb[1]
     s.bind_handler(s, b)
@@ -318,11 +318,10 @@ def n_opcode_do_bind_add_addr_uleb(s, b, i, blob):
 
 
 def n_opcode_do_bind_add_addr_imm_scaled(s, b, i, blob):
-    l.debug("DO_BIND_ADD_ADDR_IMM_SCALED @ 0x{0:X}: {1}".format(s.index, i))
+    l.debug("DO_BIND_ADD_ADDR_IMM_SCALED @ %#x: %d", s.index, i)
     if s.address >= s.seg_end_address:
-        l.error("DO_BIND_ADD_ADDR_IMM_SCALED @ 0x{0:X}: address >= seg_end_address (0x{1:X}>=0x{2:X})".format(s.index,
-                                                                                                              s.address,
-                                                                                                              s.seg_end_address))
+        l.error("DO_BIND_ADD_ADDR_IMM_SCALED @ %#x: address >= seg_end_address (%#x>=%#x)",
+                s.index, s.address, s.seg_end_address)
         raise CLEInvalidBinaryError()
     s.bind_handler(s, b)
     # this is done AFTER binding in preparation for the NEXT step
@@ -336,11 +335,11 @@ def n_opcode_do_bind_uleb_times_skipping_uleb(s, b, i, blob):
     skip = read_uleb(blob, s.index)
     s.index += skip[1]
     l.debug(
-        "DO_BIND_ULEB_TIMES_SKIPPING_ULEB @ 0x{0:X}: {1},{2}".format(s.index - skip[1] - count[1], count[0], skip[0]))
+        "DO_BIND_ULEB_TIMES_SKIPPING_ULEB @ %#x: %d,%d", s.index - skip[1] - count[1], count[0], skip[0])
     for i in range(0, count[0]):
         if s.address >= s.seg_end_address:
-            l.error("DO_BIND_ADD_ADDR_IMM_SCALED @ 0x{0:X}: address >= seg_end_address (0x{1:X}>=0x{2:X})".format(
-                s.index - skip[1] - count[1], s.address, s.seg_end_address))
+            l.error("DO_BIND_ADD_ADDR_IMM_SCALED @ %#x: address >= seg_end_address (%#x >= %#x)",
+                s.index - skip[1] - count[1], s.address, s.seg_end_address)
             raise CLEInvalidBinaryError()
         s.bind_handler(s, b)
         s.add_address_ov(s.address, skip[0] + s.sizeof_intptr_t)
@@ -356,10 +355,10 @@ def default_binding_handler(state, binary):
     # TODO: A lookup structure of some kind would be nice (see __init__)
     matches = [s for s in binary.symbols if s.name == state.sym_name and s.library_ordinal == state.lib_ord]
     if len(matches) > 1:
-        l.error("Cannot bind: More than one match for ('{0}',{1})".format(state.sym_name, state.lib_ord))
+        l.error("Cannot bind: More than one match for (%r,%d)", state.sym_name, state.lib_ord)
         raise CLEInvalidBinaryError()
     elif len(matches) < 1:
-        l.error("Cannot bind: No match for ('{0}',{1})".format(state.sym_name, state.lib_ord))
+        l.error("Cannot bind: No match for (%r,%d)", state.sym_name, state.lib_ord)
         raise CLEInvalidBinaryError()
 
     symbol = matches[0]
@@ -367,22 +366,23 @@ def default_binding_handler(state, binary):
 
     value = symbol.addr + state.addend
     if state.binding_type == 1:  # POINTER
-        l.info("Updating address 0x{1:X} with symobl '{0}' @ 0x{2:X}".format(state.sym_name, location, value))
-        binary.memory.write_bytes(location,
-                                  struct.pack(binary.struct_byteorder + ("Q" if binary.is_64bit else "I"), value))
+        l.info("Updating address %#x with symobl %r @ %#x", state.sym_name, location, value)
+        binary.memory.write_bytes(
+                location,
+                struct.pack(binary.struct_byteorder + ("Q" if binary.arch.bits == 64 else "I"), value))
         symbol.bind_xrefs.append(location)
     elif state.binding_type == 2:  # ABSOLUTE32
         location_32 = location % (2 ** 32)
         value_32 = value % (2 ** 32)
-        l.info("Updating address 0x{1:X} with symobl '{0}' @ 0x{2:X}".format(state.sym_name, location_32, value_32))
+        l.info("Updating address %#x with symobl %r @ %#x", state.sym_name, location_32, value_32)
         binary.memory.write_bytes(location_32, struct.pack(binary.struct_byteorder + "I", value_32))
         symbol.bind_xrefs.append(location_32)
     elif state.binding_type == 3:  # PCREL32
         location_32 = location % (2 ** 32)
         value_32 = (value - (location + 4)) % (2 ** 32)
-        l.info("Updating address 0x{1:X} with symobl '{0}' @ 0x{2:X}".format(state.sym_name, location_32, value_32))
+        l.info("Updating address %#x with symobl %r @ %#x", state.sym_name, location_32, value_32)
         binary.memory.write_bytes(location_32, struct.pack(binary.struct_byteorder + "I", value_32))
         symbol.bind_xrefs.append(location_32)
     else:
-        l.error("Unknown BIND_TYPE: {0}".format(type))
+        l.error("Unknown BIND_TYPE: %d", state.binding_type)
         raise CLEInvalidBinaryError()
