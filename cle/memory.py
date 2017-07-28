115a9fd2fad339b853c4349ac47a6c141e537904
Yan <yans@yancomm.net>
2017-07-27 18:15:28 -0700

    small fix for fix

--- cle/memory.py
+++ cle/memory.py
@@ -283,7 +283,7 @@ class Clemory(object):
 
         self._cbackers = [ ]
         for start, data in strides:
-            cbacker = ffi.new("unsigned short [%d]" % len(data), map(ord, data))
+            cbacker = ffi.new("unsigned short [%d]" % len(data), data)
             self._cbackers.append((start, cbacker))
 
     @property
