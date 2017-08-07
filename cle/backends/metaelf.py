d0605e2aa217091febe69fedf07172d1756f753d
Steven Lavoie <steve.l@digitaloperatives.com>
2017-08-07 12:31:58 -0500

    Use elftools parser for ELFOSABI

--- cle/backends/metaelf.py
+++ cle/backends/metaelf.py
@@ -6,6 +6,7 @@ from . import Backend
 from ..address_translator import AT
 from ..errors import CLEOperationError
 from ..utils import stream_or_path
+from elftools.elf.descriptions import describe_ei_osabi
 
 __all__ = ('MetaELF',)
 
@@ -15,7 +16,7 @@ class MetaELF(Backend):
     """
     def __init__(self, *args, **kwargs):
         super(MetaELF, self).__init__(*args, **kwargs)
-        self.os = "unix"  #TODO: FIXME: HACK: Put a real parsing of the OS in the elf header here some day
+        self.os = describe_ei_osabi(elftools.elf.elffile.ELFFile(self.binary_stream).header.e_ident.EI_OSABI)
         self._plt = {}
         self.elfflags = 0
         self.ppc64_initial_rtoc = None
