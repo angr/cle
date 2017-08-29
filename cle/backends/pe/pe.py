f74bca206aa74a93c8f73064c5c4269f4a60b1ba
Steven Lavoie <steve.l@digitaloperatives.com>
2017-08-29 13:59:00 -0500

    Add IMAGE_REL_BASED_ABSOLUTE as a relocation for clarity

--- cle/backends/pe/pe.py
+++ cle/backends/pe/pe.py
@@ -9,7 +9,7 @@ from .regions import PESection
 from .. import register_backend, Backend
 from ...address_translator import AT
 from .relocation import get_relocation
-from .relocation.generic import DllImport, IMAGE_REL_BASED_HIGHADJ
+from .relocation.generic import DllImport, IMAGE_REL_BASED_HIGHADJ, IMAGE_REL_BASED_ABSOLUTE
 
 l = logging.getLogger('cle.pe')
 
@@ -179,7 +179,8 @@ class PE(Backend):
         # Handle special cases first
 
         if reloc_type == 0:         # 0 simply means "ignore this relocation"
-            return None
+            reloc = IMAGE_REL_BASED_ABSOLUTE(owner=self, symbol=symbol, addr=addr, resolvewith=resolvewith)
+            return reloc
         if reloc_type is None:      # for DLL imports
             reloc = DllImport(owner=self, symbol=symbol, addr=addr, resolvewith=resolvewith)
             return reloc
