0eda3ee7316181baf2959ec4ae46c9b071f1aacb
r0rshark <lord.fontana@gmail.com>
2018-03-12 15:11:01 -0700

    refactored GDBConcreteTarget in AvatarGDBConcreteTarget
    Signed-off-by: r0rshark <lord.fontana@gmail.com>

--- tests/test_concrete_clemory.py
+++ tests/test_concrete_clemory.py
@@ -1,13 +1,10 @@
 import nose.tools
 import cle
 import os
-from angr_targets import GDBConcreteTarget
+from angr_targets import AvatarGDBConcreteTarget
 from avatar2 import *
 import subprocess
 from cle.concrete_memory import ConcreteClemory
-
-
-
 
 binary = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                           os.path.join('..', '..', 'binaries','tests','x86_64','fauxware'))
@@ -24,8 +21,7 @@ def test_concrete_clemory_read_bytes():
     clemory.add_backer(0, "Go away!")
     cle_msg = clemory.read_bytes(0, 8)
 
-
-    concrete_target = GDBConcreteTarget(archs.x86.X86_64, "127.0.0.1", 1234)
+    concrete_target = AvatarGDBConcreteTarget(archs.x86.X86_64, "127.0.0.1", 1234)
     concrete_clemory = ConcreteClemory(concrete_target)
     cle_msg_concrete = concrete_clemory.read_bytes(0x40090C, 8)
 
@@ -36,15 +32,13 @@ def test_concrete_clemory_read_bytes():
 def test_concrete_clemory_get_byte():
     clemory = cle.Clemory(None, root=True)
     clemory.add_backer(0, "Go away!")
-    print("get byte")
     cle_byte = clemory.get_byte(0)
 
-    print("concrete get byte")
-    concrete_target = GDBConcreteTarget(archs.x86.X86_64, "127.0.0.1", 1234)
+    concrete_target = AvatarGDBConcreteTarget(archs.x86.X86_64, "127.0.0.1", 1234)
     concrete_clemory = ConcreteClemory(concrete_target)
     cle_byte_concrete = concrete_clemory.get_byte(0x40090C)
 
-    nose.tools.assert_true(cle_byte == cle_byte_concrete and type(cle_byte_concrete) == type(cle_byte_concrete),"AAAAAAAAAAAA")
+    nose.tools.assert_true(cle_byte == cle_byte_concrete and type(cle_byte_concrete) == type(cle_byte_concrete))
     concrete_target.exit()
 
 @nose.tools.with_setup(setup_func)
@@ -54,7 +48,7 @@ def test_concrete_clemory_read():
     clemory.add_backer(0, "Go away!")
     cle_read = clemory.read(8)
 
-    concrete_target = GDBConcreteTarget(archs.x86.X86_64, "127.0.0.1", 1234)
+    concrete_target = AvatarGDBConcreteTarget(archs.x86.X86_64, "127.0.0.1", 1234)
     concrete_clemory = ConcreteClemory(concrete_target)
 
     concrete_clemory.seek(0x40090C)
