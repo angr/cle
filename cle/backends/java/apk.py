31405a0862a4b7331a2b83f7ed45b77470ab0401
Thorsten Eisenhofer <thorsten.eisenhofer@rub.de>
2018-09-11 17:23:04 -0700

    Improve support for APKs

--- cle/backends/java/apk.py
+++ cle/backends/java/apk.py
@@ -1,53 +1,140 @@
 import logging
+import os
+import tempfile
+from zipfile import ZipFile
 
 from .. import register_backend
 from .soot import Soot
 
-
 l = logging.getLogger("cle.backends.apk")
+
+# Default list of JNI archs (in descending order of preference)
+# => specifies which arch should be used for loading native libs from the APK
+default_jni_archs = ['x86', 'armeabi', 'armeabi-v7a', 'x86_64', 'arm64-v8a']
 
 
 class Apk(Soot):
-
     """
     Backend for lifting Apk's to Soot.
     """
 
     is_default = True  # let CLE automatically use this backend
 
-    def __init__(self, path, main_class=None, loader=None,
-                 native_libs=None, native_libs_ld_path=None, **options):
+    def __init__(self, apk_path, entry_point=None, android_sdk=None, supported_jni_archs=None,
+                 jni_libs=None, jni_libs_ld_path=None, **options):
         """
-        :param main_class: If no main class is specified, we try to parse it from the manifest.
+        :param apk_path:                Path to APK.
+        :param android_sdk:             Path to Android SDK folder (e.g. "/home/angr/android/platforms")
+
+        The following parameters are optional
+
+        :param entry_point:             Fully qualified name of method that should be used as the entry point.
+        :param supported_jni_archs:     List of supported JNI architectures (ABIs) in descending order of preference.
+        :param jni_libs:                Name(s) of JNI libs to load (if any). If not specified, we try to extract
+                                        JNI libs from the APK.
+        :param jni_libs_ld_path:        Path(s) where to find libs defined by param jni_libs.
+                                        Note: Directory of the APK is added by default.
         """
 
-        if native_libs or native_libs_ld_path:
-            l.warning("Native libraries are set automatically and given "
-                      "parameters get overwritten.")
-            # TODO parse apk and match native libs
-            # * Usually libs are included for various archs in the apks
-            #   => might look for a default arch (e.g. ARM) + fallbacks if not available + user option
-            # * Problem: dependencies are specified by a path to a file
-            #            => Option 1: https://github.com/angr/cle/issues/123
-            #            => Option 2: extract apk to a tmp folder
+        l.info("Loading APK from %s ...", apk_path)
 
-        if not main_class:
-            # TODO find main class
+        if not android_sdk:
+            raise ValueError('\nPath to Android SDK must be specified explicitly, e.g.\n'
+                             '    loading_opts = { "android_sdk" : "/home/angr/android/platforms" }\n'
+                             '    proj = angr.Project("/path/to/apk/target.apk", main_opts=loading_opts)')
+
+        if not supported_jni_archs:
+            supported_jni_archs = default_jni_archs
+
+        # if jni libs are not defined by the user, we try to extract them from the APK
+        if not jni_libs:
+            l.info("No JNI libs provided. Trying to parse them from the APK.")
+            jni_libs, jni_libs_ld_path = self._extract_jni_libs(apk_path, supported_jni_archs)
+        else:
+            l.info("Using user defined JNI lib(s) %s (load path(s) %s)", jni_libs, jni_libs_ld_path)
+
+        if not entry_point:
+            # TODO find a suitable entry point
             pass
 
         # the actual lifting is done by the Soot superclass
-        # TODO it might be necessary to specify the path to android sdk
-        #      (see example apk's in pysoot project)
-        super(Apk, self).__init__(path,
-                                  main_class=main_class,
-                                  native_libs=native_libs,
-                                  native_libs_ld_path=native_libs_ld_path,
+        super(Apk, self).__init__(apk_path,
+                                  input_format='apk',
+                                  android_sdk=android_sdk,
+                                  entry_point=entry_point,
+                                  jni_libs=jni_libs,
+                                  jni_libs_ld_path=jni_libs_ld_path,
                                   **options)
+
+
+    @staticmethod
+    def _extract_jni_libs(apk_path, supported_jni_archs):
+        """
+        Extract JNI libs from APK.
+
+        If an APK uses native libraries via JNI, the APK usually include the libs compiled for
+        various architectures. This method first matches the available archs with the list of
+        supported archs and then extracts the JNI libs using one of the matched archs.
+
+        :return: Name of all extracted JNI libs together with the path to the directory used for
+                 extracting.
+        :rtype: tuple
+        """
+        with ZipFile(apk_path) as apk:
+            # Step 1: get filelist from APK
+            # => structure follows this schema:
+            #    AndroidManifest.xml
+            #    META-INF/MANIFEST.MF
+            #    classes.dex
+            #    lib/armeabi-v7a/libnative-lib.so
+            #    lib/x86/libnative-lib.so
+            filelist = apk.namelist()
+
+            # Step 2: parse name of available libs and archs
+            #         from lib paths "/lib/<jni_arch>/lib<name>.so"
+            lib_filelist = [f.split('/') for f in filelist if f.startswith('lib')]
+            jni_libs = set([lib_path[2] for lib_path in lib_filelist])
+            available_jni_archs = set([lib_path[1] for lib_path in lib_filelist])
+
+            if not jni_libs:
+                l.info("No JNI libs found.")
+                return None, None
+            l.info("Found JNI lib(s): %s",", ".join(jni_libs))
+
+            # Step 3: get the first supported jni arch that is available in the APK
+            jni_archs = [arch for arch in supported_jni_archs
+                            if  arch in available_jni_archs]
+            if not jni_archs:
+                raise ValueError("Couldn't find a supported JNI arch. Available %s. Supported %s."
+                                 "" % (available_jni_archs, supported_jni_archs))
+            jni_arch = jni_archs[0]
+            l.info("Libs are available with arch(s): %s. Picking %s.", ", ".join(available_jni_archs), jni_arch)
+
+            # Step 4: extract all used libaries from the APK
+            # TODO: implement this w/o the need of actually writing files to disk
+            #       see https://github.com/angr/cle/issues/123
+            tmp_dir = tempfile.mkdtemp()
+            for lib in jni_libs:
+                apk_file = "lib/{jni_arch}/{lib_name}".format(jni_arch=jni_arch, lib_name=lib)
+                apk.extract(apk_file, path=tmp_dir)
+            jni_libs_ld_path = os.path.join(tmp_dir, 'lib', jni_arch)
+
+            l.info("Extracted lib(s) to %s", jni_libs_ld_path)
+            return jni_libs, jni_libs_ld_path
 
     @staticmethod
     def is_compatible(stream):
-        # TODO check for android manifest and .dex classses
-        return False
-
+        # check if stream is an archive
+        if not Soot.is_zip_archive(stream):
+            return False
+        # get filelist
+        with ZipFile(stream) as apk:
+            filelist = apk.namelist()
+        # check for manifest and the .dex bytecode file
+        if 'AndroidManifest.xml' not in filelist:
+            return False
+        if 'classes.dex' not in filelist:
+            return False
+        return True
 
 register_backend('apk', Apk)
