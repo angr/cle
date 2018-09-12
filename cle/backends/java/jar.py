8f8fb3d4c7210ba69404c37bbd3cee41e0c7be5a
Thorsten Eisenhofer <thorsten.eisenhofer@rub.de>
2018-09-11 17:23:04 -0700

    Improve support for APKs

--- cle/backends/java/jar.py
+++ cle/backends/java/jar.py
@@ -1,63 +1,67 @@
 import logging
-import zipfile
+from zipfile import ZipFile
 
 from .. import register_backend
 from .soot import Soot
-
 
 l = logging.getLogger("cle.backends.jar")
 
 
 class Jar(Soot):
-
     """
-    Backend for lifting Jar's to Soot.
+    Backend for lifting JARs to Soot.
     """
 
     is_default = True  # let CLE automatically use this backend
 
-    def __init__(self, path, main_class=None, loader=None, native_libs=None,
-                 native_libs_ld_path=None, **options):
+    def __init__(self, jar_path, entry_point=None, jni_libs=None, jni_libs_ld_path=None, **options):
         """
-        :param main_class:              If no main class is specified, we try to parse it from the manifest.
+        :param jar_path:                Path to JAR.
 
-        :param additional_jars:         See Soot Backend.
-        :param additional_jar_roots:    See Soot Backend.
+        The following parameters are optional
 
-        :param native_libs:             See Soot Backend.
-        :param native_libs_ld_path:     See Soot Backend.
+        :param entry_point:             Fully qualified name of method that should be used as the entry point.
+                                        If not specified, we try to parse it from the manifest.
+        :param additional_jars:         Additional JARs.
+        :param additional_jar_roots:    Additional JAR roots.
+        :param jni_libs:                Name(s) of JNI libs to load (if any).
+        :param jni_libs_ld_path:        Path(s) where to find libs defined by param jni_libs.
+                                        Note: Directory of the JAR is added by default.
         """
 
-        if native_libs and not loader.auto_load_libs:
-            l.warning("To load native libraries, auto_load_libs must be true.")
+        l.debug("Loading JAR from %s ...", jar_path)
 
-        if not main_class:
-            # try to parse main class from the manifest
-            self.manifest = self.get_manifest(path)
+        if not entry_point:
+            # try to parse main class from manifest
+            self.manifest = self.get_manifest(jar_path)
             main_class = self.manifest.get('Main-Class', None)
+            if main_class:
+                entry_point = main_class + "." + "main"
 
         # the actual lifting is done by the Soot superclass
-        super(Jar, self).__init__(path,
-                                  main_class=main_class,
-                                  native_libs=native_libs,
-                                  native_libs_ld_path=native_libs_ld_path,
+        super(Jar, self).__init__(jar_path,
+                                  input_format='jar',
+                                  entry_point=entry_point,
+                                  jni_libs=jni_libs,
+                                  jni_libs_ld_path=jni_libs_ld_path,
                                   **options)
 
     @staticmethod
     def is_compatible(stream):
-        identstring = stream.read(4)
-        stream.seek(0)
-        if identstring.startswith('\x50\x4b\x03\x04') and Jar.is_jar(stream):
-            return True
-        return False
-
-    @staticmethod
-    def is_jar(stream):
-        z = zipfile.ZipFile(stream)
-        for f in z.filelist:
-            if f.filename == 'META-INF/MANIFEST.MF':
-                return True
-        return False
+        # check if stream is an archive
+        if not Soot.is_zip_archive(stream):
+            return False
+        # get filelist
+        with ZipFile(stream) as jar:
+            filelist = jar.namelist()
+        # check for manifest and if a least one java class
+        # file is available
+        if 'META-INF/MANIFEST.MF' not in filelist:
+            return False
+        class_files = [f for f in filelist if f.endswith('.class')]
+        if len(class_files) == 0:
+            return False
+        return True
 
     def get_manifest(self, binary_path=None):
         """
@@ -67,25 +71,14 @@ class Jar(Soot):
         :rtype:  dict
         """
         path = binary_path if binary_path else self.binary
-        z = zipfile.ZipFile(path)
-
-        for f in z.filelist:
-            if f.filename == 'META-INF/MANIFEST.MF':
-                break
-
-        manifest = z.open('META-INF/MANIFEST.MF', "r")
-
-        data = {}
-        for l in manifest.readlines():
-            if ':' in l:
-                key, value = l.split(':')
-                key = key.strip()
-                value = value.strip()
-                data[key] = value
-
-        manifest.close()
-
-        return data
+        with ZipFile(path) as jar:
+            manifest = jar.open('META-INF/MANIFEST.MF', "r")
+            data = {}
+            for line in manifest.readlines():
+                if ':' in line:
+                    key, value = line.split(':')
+                    data[key.strip()] = value.strip()
+            return data
 
 
 register_backend('jar', Jar)
