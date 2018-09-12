ee7031eb85370b9dfc94f2444affce835d86dc94
Thorsten Eisenhofer <thorsten.eisenhofer@rub.de>
2018-09-11 17:23:04 -0700

    Improve support for APKs

--- cle/backends/java/soot.py
+++ cle/backends/java/soot.py
@@ -1,8 +1,9 @@
 
 import logging
+import time
 
-import archinfo
-from archinfo.arch_soot import SootAddressDescriptor, SootMethodDescriptor
+from archinfo.arch_soot import (ArchSoot, SootAddressDescriptor,
+                                SootMethodDescriptor)
 
 from .. import Backend
 from ...errors import CLEError
@@ -13,35 +14,21 @@ try:
 except ImportError:
     pysoot = None
 
-
-_l = logging.getLogger("cle.backends.soot")
+l = logging.getLogger("cle.backends.soot")
 
 
 class Soot(Backend):
-
     """
-    The basis loader class for lifting Java code from Jar's and Apk's to Soot.
+    The basis backend for lifting and loading bytecode from JARs and APKs to Soot IR.
     """
 
-    def __init__(self, path, main_class=None,
+    def __init__(self, path, entry_point=None, input_format=None,
                  additional_jars=None, additional_jar_roots=None,
-                 native_libs_ld_path=None, native_libs=None,
-                 **kwargs):
-        """
-        :param path:                    Path to the main jar or apk.
-
-        The following parameters are optional
-
-        :param main_class:              Name of class containing the main method, which should be used as entry point.
-        :param additional_jars:         Additional Jars.
-        :param additional_jar_roots:    Additional Jar roots.
-        :param native_libs:             Name(s) if libraries containing native code components (JNI)
-        :param native_libs_ld_path:     Path(s) where to find native libraries. Note: Requires use_system_libs=True
-        """
+                 jni_libs_ld_path=None, jni_libs=None,
+                 android_sdk=None, **kwargs):
 
         if not pysoot:
-            raise ImportError('Cannot import PySoot. The Soot backend requires PySoot to function. '
-                              'Please install PySoot first.')
+            raise ImportError('Cannot import PySoot. The Soot backend requires PySoot.')
 
         if kwargs.get('has_memory', False):
             raise CLEError('The parameter "has_memory" must be False for Soot backend.')
@@ -49,34 +36,39 @@ class Soot(Backend):
         super(Soot, self).__init__(path, has_memory=False, **kwargs)
 
         # load the classes
+        l.debug("Lifting to Soot IR ...")
+        start_time = time.time()
         pysoot_lifter = Lifter(path,
+                               input_format=input_format,
+                               android_sdk=android_sdk,
                                additional_jars=additional_jars,
-                               additional_jar_roots=additional_jar_roots
-                               )
+                               additional_jar_roots=additional_jar_roots)
+        end_time = time.time()
+        l.debug("Lifting completed in %ds", round(end_time - start_time, 2))
         self._classes = pysoot_lifter.classes
 
         # find entry method
-        try:
-            main_method = self.get_soot_method("main", main_class)
-            main_method_descriptor = SootMethodDescriptor.from_soot_method(main_method)
-            entry = SootAddressDescriptor(main_method_descriptor, 0, 0)
-        except CLEError:
-            _l.warning('Failed to identify the entry (the Main method).')
-            entry = None
+        if entry_point:
+            try:
+                ep_method = self.get_soot_method(entry_point)
+                ep_method_descriptor = SootMethodDescriptor.from_soot_method(ep_method)
+                self._entry = SootAddressDescriptor(ep_method_descriptor, 0, 0)
+                l.debug("Entry point set to %s", self._entry)
+            except CLEError:
+                l.warning("Couldn't find entry point %s.", entry_point)
+                self._entry = None
 
-        self._entry = entry
         self.os = 'javavm'
         self.rebase_addr = None
-        self.set_arch(archinfo.arch_from_id('soot'))
+        self.set_arch(ArchSoot())
 
-        if native_libs:
-            # automatically load nativ libraries (with CLE) by adding them as a dependency of this object
-            self.deps += [native_libs] if type(native_libs) in (str, unicode) else native_libs
+        if jni_libs:
+            # native libraries are getting loaded by adding them as a dependency of this object
+            self.deps += [jni_libs] if type(jni_libs) in (str, unicode) else jni_libs
             # if available, add additional load path(s)
-            if native_libs_ld_path:
-                path_list = [native_libs_ld_path] if type(native_libs_ld_path) in (str, unicode) else native_libs_ld_path
+            if jni_libs_ld_path:
+                path_list = [jni_libs_ld_path] if type(jni_libs_ld_path) in (str, unicode) else jni_libs_ld_path
                 self.extra_load_path += path_list
-            # JNI support enables to switch between the Java SimOS and the one used by native libs
             self.jni_support = True
         else:
             self.jni_support = False
@@ -115,9 +107,9 @@ class Soot(Backend):
 
     def get_soot_class(self, cls_name, none_if_missing=False):
         """
-        Get a Soot class object.
+        Get Soot class object.
 
-        :param str cls_name: Name of the class to get.
+        :param str cls_name: Name of the class.
         :return:             The class object.
         :rtype:              pysoot.soot.SootClass
         """
@@ -131,12 +123,11 @@ class Soot(Backend):
 
     def get_soot_method(self, thing, class_name=None, params=(), none_if_missing=False):
         """
-        Get a Soot method object.
+        Get Soot method object.
 
         :param thing:           Descriptor or the method, or name of the method.
         :param str class_name:  Name of the class. If not specified, class name can be parsed from method_name.
-        :return:                An iterator of all SootMethod objects that satisfy the criteria.
-        :rtype:                 iterator
+        :return:                Soot method that satisfy the criteria.
         """
 
         # Step 1: Parse input
@@ -157,7 +148,7 @@ class Soot(Backend):
                     class_name = method_name[:last_dot]
                     method_name = method_name[last_dot + 1:]
                 else:
-                    raise CLEError('Unknown class name for the method.')
+                    raise ValueError('Cannot parse class name from method %s.' % method_name)
 
             method_description = {
                 'class_name': class_name,
@@ -166,8 +157,7 @@ class Soot(Backend):
             }
 
         else:
-            raise TypeError('Unsupported type "%s" for the first argument.'
-                            ''% type(thing))
+            raise TypeError('Unsupported type "%s" for the first argument.' % thing)
 
         # Step 2: Load class containing the method
         try:
@@ -182,16 +172,16 @@ class Soot(Backend):
         methods = [soot_method for soot_method in cls.methods
                    if self._description_matches_soot_method(soot_method, **method_description)]
 
-        if len(methods) == 0:
+        if not methods:
             if none_if_missing:
                 return None
             else:
                 raise CLEError('Method with description %s does not exist in class %s.'
-                               '' % (method_description, class_name))
+                               % (method_description, class_name))
 
         if len(methods) > 1:
             # Warn if we found several matching methods
-            _l.warning('Method with description %s is ambiguous in class %s.',
+            l.warning('Method with description %s is ambiguous in class %s.',
                        method_description, class_name)
 
         return methods[0]
@@ -214,3 +204,9 @@ class Soot(Backend):
             for method in cls.methods:
                 if method.name == "main":  # TODO: Check more attributes
                     yield method
+
+    @staticmethod
+    def is_zip_archive(stream):
+        identstring = stream.read(4)
+        stream.seek(0)
+        return identstring.startswith('\x50\x4b\x03\x04')
