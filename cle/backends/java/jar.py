89c84c1ea9ee6cd29b37c5d7359cf39a0e15dfde
Audrey Dutcher <audrey@rhelmot.io>
2019-02-19 13:40:28 -0700

    Set default entry point params

--- cle/backends/java/jar.py
+++ cle/backends/java/jar.py
@@ -14,7 +14,7 @@ class Jar(Soot):
 
     is_default = True  # let CLE automatically use this backend
 
-    def __init__(self, jar_path, entry_point=None, entry_point_params=(), jni_libs=None, jni_libs_ld_path=None, **options):
+    def __init__(self, jar_path, entry_point=None, entry_point_params=('java.lang.String[]',), jni_libs=None, jni_libs_ld_path=None, **options):
         """
         :param jar_path:                Path to JAR.
 
@@ -37,8 +37,6 @@ class Jar(Soot):
             main_class = self.manifest.get('Main-Class', None)
             if main_class:
                 entry_point = main_class + "." + "main"
-                # FIXME: Make this parametric
-                entry_point_params = ('java.lang.String[]', )
 
         # the actual lifting is done by the Soot superclass
         super(Jar, self).__init__(jar_path,
