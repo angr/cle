c6c120cf5bb9fc1b86359f7743670556253d8dc1
Audrey Dutcher <audrey@rhelmot.io>
2019-04-05 12:51:13 -0700

    Add version number

--- cle/__init__.py
+++ cle/__init__.py
@@ -5,6 +5,8 @@ produce an address space where that program is loaded and ready to run.
 The primary interface to CLE is the Loader class.
 """
 
+__version__ = (8, 19, 2, 4)
+
 if bytes is str:
     raise Exception("This module is designed for python 3 only. Please install an older version to use python 2.")
 
