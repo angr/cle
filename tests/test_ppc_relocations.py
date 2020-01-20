6657e2da9d14547424d7d33a2710070a61c8b373
Morgan <ke5vfk@gmail.com>
2020-01-20 17:28:12 +0000

    A shot in the dark

--- tests/test_ppc_relocations.py
+++ tests/test_ppc_relocations.py
@@ -1,6 +1,6 @@
 import os
 import cle
-from nose.tools import assert_equal
+import nose
 
 test_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries', 'tests'))
 path = os.path.join(test_location, "ppc", "partial.o")
@@ -18,21 +18,21 @@ def test_ppc_rel24_relocation():
     # Verify that a faulty branch-and-link instruction operates correctly.
     # Expected bytes: 4b ff ff 05
     byte_value = l.memory.load(0x414838, 4)
-    assert_equal(byte_value, b'K\xff\xff\x05')
+    nose.tools.assert_equal(byte_value, b'K\xff\xff\x05')
 
     # Verify that the symbol in the bl instruction above is correct.
     goodG2B1Source = l.find_symbol("goodG2B1Source")
-    assert_equal(goodG2B1Source.relative_addr, 83772)
+    nose.tools.assert_equal(goodG2B1Source.relative_addr, 83772)
 
     # Verify relocated symbol exists in addition to its calculated value.
     found_symbol = False
     for r in relocations:
         if r.symbol.name == "_Znwj" and r.__class__ == ppc_backend.R_PPC_REL24:
             found_symbol = True
-            assert_equal(r.value, 1220440101)
+            nose.tools.assert_equal(r.value, 1220440101)
             break
 
-    assert_equal(found_symbol, True)
+    nose.tools.assert_equal(found_symbol, True)
 
 
 def test_ppc_addr16_ha_relocation():
@@ -46,11 +46,11 @@ def test_ppc_addr16_ha_relocation():
     for r in relocations:
         if r.symbol.name == "CWE123_Write_What_Where_Condition__fgets_22_goodG2B2Global" and r.__class__ == ppc_backend.R_PPC_ADDR16_HA:
             found_symbol = True
-            assert_equal(r.value, 67)
-            assert_equal(r.relative_addr, 29682)
+            nose.tools.assert_equal(r.value, 67)
+            nose.tools.assert_equal(r.relative_addr, 29682)
             break
 
-    assert_equal(found_symbol, True)
+    nose.tools.assert_equal(found_symbol, True)
 
 
 def test_ppc_addr16_lo_relocation():
@@ -64,11 +64,11 @@ def test_ppc_addr16_lo_relocation():
     for r in relocations:
         if r.symbol.name == "CWE123_Write_What_Where_Condition__listen_socket_68_badData" and r.__class__ == ppc_backend.R_PPC_ADDR16_LO:
             found_symbol = True
-            assert_equal(r.value, 4)
-            assert_equal(r.relative_addr, 6930)
+            nose.tools.assert_equal(r.value, 4)
+            nose.tools.assert_equal(r.relative_addr, 6930)
             break
 
-    assert_equal(found_symbol, True)
+    nose.tools.assert_equal(found_symbol, True)
 
 
 if __name__ == "__main__":
