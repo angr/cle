4f133cb1c9bb389156175268eb1b989d76e4d280
W. Michael Petullo <mike@flyn.org>
2020-05-28 15:13:10 -0400

    Adjust minidump dependency to >= 0.0.10

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         'sortedcontainers>=2.0',
     ],
     extras_require={
-        "minidump": ["minidump==0.0.10"],
+        "minidump": ["minidump>=0.0.10"],
         "xbe": ["pyxbe==0.0.2"],
         "ar": ["arpy==1.1.1"],
     }
