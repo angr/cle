97ea82d2a2d59669b6e88a52b083e6813aca33d0
kordood <gigacms@gmail.com>
2021-08-10 04:50:43 +0000

    Reformat lifecycle

--- cle/backends/java/android_lifecycle.py
+++ cle/backends/java/android_lifecycle.py
@@ -1,127 +1,103 @@
-class ComponentClass:
-    ACTIVITYCLASS = "android.app.Activity"
-    SERVICECLASS = "android.app.Service"
-    BROADCASTRECEIVERCLASS = "android.content.BroadcastReceiver"
-    CONTENTPROVIDERCLASS = "android.content.ContentProvider"
-    GCMBASEINTENTSERVICECLASS = "com.google.android.gcm.GCMBaseIntentService"
-    GCMLISTENERSERVICECLASS = "com.google.android.gms.gcm.GcmListenerService"
-    APPLICATIONCLASS = "android.app.Application"
-    FRAGMENTCLASS = "android.app.Fragment"
-    SUPPORTFRAGMENTCLASS = "android.support.v4.app.Fragment"
-    ANDROIDXFRAGMENTCLASS = "androidx.fragment.app.Fragment"
-    SERVICECONNECTIONINTERFACE = "android.content.ServiceConnection"
-    MAPACTIVITYCLASS = "com.google.android.maps.MapActivity"
-
-    APPCOMPATACTIVITYCLASS_V4 = "android.support.v4.app.AppCompatActivity"
-    APPCOMPATACTIVITYCLASS_V7 = "android.support.v7.app.AppCompatActivity"
-    APPCOMPATACTIVITYCLASS_X = "androidx.appcompat.app.AppCompatActivity"
-
-    COMPONENTCALLBACKSINTERFACE = "android.content.ComponentCallbacks"
-    COMPONENTCALLBACKS2INTERFACE = "android.content.ComponentCallbacks2"
-
-
-class ActivityCallback:
-    ACTIVITY_ONCREATE = "onCreate(android.os.Bundle)"
-    ACTIVITY_ONSTART = "onStart()"
-    ACTIVITY_ONRESTOREINSTANCESTATE = "onRestoreInstanceState(android.os.Bundle)"
-    ACTIVITY_ONPOSTCREATE = "onPostCreate(android.os.Bundle)"
-    ACTIVITY_ONRESUME = "onResume()"
-    ACTIVITY_ONPOSTRESUME = "onPostResume()"
-    ACTIVITY_ONCREATEDESCRIPTION = "onCreateDescription()"
-    ACTIVITY_ONSAVEINSTANCESTATE = "onSaveInstanceState(android.os.Bundle)"
-    ACTIVITY_ONPAUSE = "onPause()"
-    ACTIVITY_ONSTOP = "onStop()"
-    ACTIVITY_ONRESTART = "onRestart()"
-    ACTIVITY_ONDESTROY = "onDestroy()"
-    ACTIVITY_ONATTACHFRAGMENT = "onAttachFragment(android.app.Fragment)"
-
-
-class ServiceCallback:
-    SERVICE_ONCREATE = "onCreate()"
-    SERVICE_ONSTART1 = "onStart(android.content.Intent,int)"
-    SERVICE_ONSTART2 = "onStartCommand(android.content.Intent,int,int)"
-    SERVICE_ONBIND = "onBind(android.content.Intent)"
-    SERVICE_ONREBIND = "onRebind(android.content.Intent)"
-    SERVICE_ONUNBIND = "onUnbind(android.content.Intent)"
-    SERVICE_ONDESTROY = "onDestroy()"
-
-
-class RecieverCallback:
-    BROADCAST_ONRECEIVE = "onReceive(android.content.Context,android.content.Intent)"
-
-
-class ProviderCallback:
-    CONTENTPROVIDER_ONCREATE = "onCreate()"
-    CONTENTPROVIDER_INSERT = "insert(android.net.Uri,android.content.ContentValues)"
-    CONTENTPROVIDER_QUERY = "android.database.Cursor query(android.net.Uri,java.lang.String[],java.lang.String,java.lang.String[],java.lang.String)"
-    CONTENTPROVIDER_UPDATE = "update(android.net.Uri,android.content.ContentValues,java.lang.String,java.lang.String[])"
-    CONTENTPROVIDER_DELETE = "delete(android.net.Uri,java.lang.String,java.lang.String[])"
-    CONTENTPROVIDER_GETTYPE = "getType(android.net.Uri)"
-
-
-class GCMIntentServiceCallback:
-    GCMINTENTSERVICE_ONDELETEDMESSAGES = "onDeletedMessages(android.content.Context,int)"
-    GCMINTENTSERVICE_ONERROR = "onError(android.content.Context,java.lang.String)"
-    GCMINTENTSERVICE_ONMESSAGE = "onMessage(android.content.Context,android.content.Intent)"
-    GCMINTENTSERVICE_ONRECOVERABLEERROR = "onRecoverableError(android.content.Context,java.lang.String)"
-    GCMINTENTSERVICE_ONREGISTERED = "onRegistered(android.content.Context,java.lang.String)"
-    GCMINTENTSERVICE_ONUNREGISTERED = "onUnregistered(android.content.Context,java.lang.String)"
-
-
-class GCMListenerServiceCallback:
-    GCMLISTENERSERVICE_ONDELETEDMESSAGES = "onDeletedMessages()"
-    GCMLISTENERSERVICE_ONMESSAGERECEIVED = "onMessageReceived(java.lang.String,android.os.Bundle)"
-    GCMLISTENERSERVICE_ONMESSAGESENT = "onMessageSent(java.lang.String)"
-    GCMLISTENERSERVICE_ONSENDERROR = "onSendError(java.lang.String,java.lang.String)"
-
-
-class ApplicationCallback:
-    APPLICATION_ONCREATE = "onCreate()"
-    APPLICATION_ONTERMINATE = "onTerminate()"
-
-    SERVICECONNECTION_ONSERVICECONNECTED = "onServiceConnected(android.content.ComponentName,android.os.IBinder)"
-    SERVICECONNECTION_ONSERVICEDISCONNECTED = "onServiceDisconnected(android.content.ComponentName)"
-
-    ACTIVITYLIFECYCLECALLBACKSINTERFACE = "android.app.Application$ActivityLifecycleCallbacks"
-    ACTIVITYLIFECYCLECALLBACK_ONACTIVITYSTARTED = "onActivityStarted(android.app.Activity)"
-    ACTIVITYLIFECYCLECALLBACK_ONACTIVITYSTOPPED = "onActivityStopped(android.app.Activity)"
-    ACTIVITYLIFECYCLECALLBACK_ONACTIVITYSAVEINSTANCESTATE = "onActivitySaveInstanceState(android.app.Activity,android.os.Bundle)"
-    ACTIVITYLIFECYCLECALLBACK_ONACTIVITYRESUMED = "onActivityResumed(android.app.Activity)"
-    ACTIVITYLIFECYCLECALLBACK_ONACTIVITYPAUSED = "onActivityPaused(android.app.Activity)"
-    ACTIVITYLIFECYCLECALLBACK_ONACTIVITYDESTROYED = "onActivityDestroyed(android.app.Activity)"
-    ACTIVITYLIFECYCLECALLBACK_ONACTIVITYCREATED = "onActivityCreated(android.app.Activity,android.os.Bundle)"
-
-    COMPONENTCALLBACKS_ONLOWMEMORY = "onLowMemory()"
-    COMPONENTCALLBACKS_ONCONFIGURATIONCHANGED = "onConfigurationChanged(android.content.res.Configuration)"
-
-    COMPONENTCALLBACKS2_ONTRIMMEMORY = "onTrimMemory(int)"
-
-
-class FragmentCallback:
-    FRAGMENT_ONCREATE = "onCreate(android.os.Bundle)"
-    FRAGMENT_ONATTACH = "onAttach(android.app.Activity)"
-    FRAGMENT_ONCREATEVIEW = "onCreateView(android.view.LayoutInflater,android.view.ViewGroup,android.os.Bundle)"
-    FRAGMENT_ONVIEWCREATED = "onViewCreated(android.view.View,android.os.Bundle)"
-    FRAGMENT_ONSTART = "onStart()"
-    FRAGMENT_ONACTIVITYCREATED = "onActivityCreated(android.os.Bundle)"
-    FRAGMENT_ONVIEWSTATERESTORED = "onViewStateRestored(android.app.Activity)"
-    FRAGMENT_ONRESUME = "onResume()"
-    FRAGMENT_ONPAUSE = "onPause()"
-    FRAGMENT_ONSTOP = "onStop()"
-    FRAGMENT_ONDESTROYVIEW = "onDestroyView()"
-    FRAGMENT_ONDESTROY = "onDestroy()"
-    FRAGMENT_ONDETACH = "onDetach()"
-    FRAGMENT_ONSAVEINSTANCESTATE = "onSaveInstanceState(android.os.Bundle)"
-
-
-component_class = ComponentClass
+component_class = [
+    "android.app.Activity",
+    "android.app.Service",
+    "android.content.BroadcastReceiver",
+    "android.content.ContentProvider",
+    "com.google.android.gcm.GCMBaseIntentService",
+    "com.google.android.gms.gcm.GcmListenerService",
+    "android.app.Application",
+    "android.app.Fragment",
+    "android.support.v4.app.Fragment",
+    "androidx.fragment.app.Fragment",
+    "android.content.ServiceConnection",
+    "com.google.android.maps.MapActivity",
+    "android.support.v4.app.AppCompatActivity",
+    "android.support.v7.app.AppCompatActivity",
+    "androidx.appcompat.app.AppCompatActivity",
+    "android.content.ComponentCallbacks",
+    "android.content.ComponentCallbacks2",
+]
 callback = {
-    'activity': ActivityCallback,
-    'service': ServiceCallback,
-    'receiver': RecieverCallback,
-    'provider': ProviderCallback,
-    'gcmintent': GCMIntentServiceCallback,
-    'gcmlistener': GCMListenerServiceCallback,
-    'application': ApplicationCallback,
-    'fragment': FragmentCallback,
-    }
+    'activity': [
+        "onCreate(android.os.Bundle)",
+        "onStart()",
+        "onRestoreInstanceState(android.os.Bundle)",
+        "onPostCreate(android.os.Bundle)",
+        "onResume()",
+        "onPostResume()",
+        "onCreateDescription()",
+        "onSaveInstanceState(android.os.Bundle)",
+        "onPause()",
+        "onStop()",
+        "onRestart()",
+        "onDestroy()",
+        "onAttachFragment(android.app.Fragment)",
+    ],
+    'service': [
+        "onCreate()",
+        "onStart(android.content.Intent,int)",
+        "onStartCommand(android.content.Intent,int,int)",
+        "onBind(android.content.Intent)",
+        "onRebind(android.content.Intent)",
+        "onUnbind(android.content.Intent)",
+        "onDestroy()",
+    ],
+    'receiver': [
+        "onReceive(android.content.Context,android.content.Intent)",
+    ],
+    'provider': [
+        "onCreate()",
+        "insert(android.net.Uri,android.content.ContentValues)",
+        "query(android.net.Uri,java.lang.String[],java.lang.String,java.lang.String[],java.lang.String)",
+        "update(android.net.Uri,android.content.ContentValues,java.lang.String,java.lang.String[])",
+        "delete(android.net.Uri,java.lang.String,java.lang.String[])",
+        "getType(android.net.Uri)",
+    ],
+    'gcmintentservice': [
+        "onDeletedMessages(android.content.Context,int)",
+        "onError(android.content.Context,java.lang.String)",
+        "onMessage(android.content.Context,android.content.Intent)",
+        "onRecoverableError(android.content.Context,java.lang.String)",
+        "onRegistered(android.content.Context,java.lang.String)",
+        "onUnregistered(android.content.Context,java.lang.String)",
+    ],
+    'gcmlistenerservice': [
+        "onDeletedMessages()",
+        "onMessageReceived(java.lang.String,android.os.Bundle)",
+        "onMessageSent(java.lang.String)",
+        "onSendError(java.lang.String,java.lang.String)",
+    ],
+    'application': [
+        "onCreate()",
+        "onTerminate()",
+        "onServiceConnected(android.content.ComponentName,android.os.IBinder)",
+        "onServiceDisconnected(android.content.ComponentName)",
+        "android.app.Application$ActivityLifecycleCallbacks",
+        "onActivityStarted(android.app.Activity)",
+        "onActivityStopped(android.app.Activity)",
+        "onActivitySaveInstanceState(android.app.Activity,android.os.Bundle)",
+        "onActivityResumed(android.app.Activity)",
+        "onActivityPaused(android.app.Activity)",
+        "onActivityDestroyed(android.app.Activity)",
+        "onActivityCreated(android.app.Activity,android.os.Bundle)",
+        "onLowMemory()",
+        "onConfigurationChanged(android.content.res.Configuration)",
+        "onTrimMemory(int)",
+    ],
+    'fragment': [
+        "onCreate(android.os.Bundle)",
+        "onAttach(android.app.Activity)",
+        "onCreateView(android.view.LayoutInflater,android.view.ViewGroup,android.os.Bundle)",
+        "onViewCreated(android.view.View,android.os.Bundle)",
+        "onStart()",
+        "onActivityCreated(android.os.Bundle)",
+        "onViewStateRestored(android.app.Activity)",
+        "onResume()",
+        "onPause()",
+        "onStop()",
+        "onDestroyView()",
+        "onDestroy()",
+        "onDetach()",
+        "onSaveInstanceState(android.os.Bundle)",
+    ]
+}
