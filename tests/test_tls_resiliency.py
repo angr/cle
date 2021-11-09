9e054bc420a9b17fa28bcee02f4419814eeff005
Fish <fish@cs.ucsb.edu>
2021-11-08 17:00:16 -0700

    TLS: Add some resiliency.

---
+++ tests/test_tls_resiliency.py
@@ -0,0 +1,19 @@
+import os
+import cle
+from unittest import TestCase
+
+test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                             os.path.join('..', '..', 'binaries', 'tests'))
+
+
+class TestTlsResiliency(TestCase):
+    def test_tls_pe_incorrect_tls_data_start(self):
+        p = os.path.join(test_location, "i386", "windows", "2.exe")
+        path_ld = cle.Loader(p)
+        assert path_ld is not None
+        th = path_ld.tls.new_thread()
+        assert th is not None
+
+
+if __name__ == '__main__':
+    TestTlsResiliency().test_tls_pe_incorrect_tls_data_start()
