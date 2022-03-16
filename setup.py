e230c39ac7f4f884454bba960c835daad5f5c557
Kevin Phoenix <kevin@kphoenix.us>
2022-03-16 13:12:45 -0700

    Add pyproject.toml (#314)

--- setup.py
+++ setup.py
@@ -1,21 +1,13 @@
-try:
-    from setuptools import setup
-    from setuptools import find_packages
-    packages = find_packages()
-except ImportError:
-    from distutils.core import setup
-    import os
-    packages = [x.strip('./').replace('/','.') for x in os.popen('find -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
-
-if bytes is str:
-    raise Exception("This module is designed for python 3 only. Please install an older version to use python 2.")
+from setuptools import setup, find_packages
 
 setup(
     name='cle',
-    description='CLE Loads Everything (at least, many binary formats!) and provides a pythonic interface to analyze what they are and what they would look like in memory.',
+    description='CLE Loads Everything (at least, many binary formats!) and '
+        'provides a pythonic interface to analyze what they are and what they '
+        'would look like in memory.',
     version='9.1.gitrolling',
     python_requires='>=3.6',
-    packages=packages,
+    packages=find_packages(),
     install_requires=[
         'pyelftools>=0.27',
         'cffi',
