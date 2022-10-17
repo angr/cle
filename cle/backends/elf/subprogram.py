7b8f56bb278ee538b7d4239f719e081f15cbd341
duchoang <hoangvanduccs@gmail.com>
2022-10-17 16:16:51 +0200

    add attr global_vars in cu and local_vars in Subprogram as describe in issue#4

--- cle/backends/elf/subprogram.py
+++ cle/backends/elf/subprogram.py
@@ -12,3 +12,14 @@ class Subprogram:
         self.name = name
         self.low_pc = low_pc
         self.high_pc = high_pc
+
+    @property
+    def local_vars(self):
+        '''
+        an alternative property for attribute local_variable that resolve
+        variable name -> Variable (Variable type)
+        '''
+        local_vars = {}
+        for local_var in self.local_variables:
+            local_vars[local_var.name] = local_var
+        return local_vars
