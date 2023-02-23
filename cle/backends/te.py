864f18e843198cfd4dc81f96abd877a4da5fb97c
Audrey Dutcher <audrey@rhelmot.io>
2023-02-23 12:13:02 -0700

    UEFI support (#368)
    * write uefi loader
    
    * Load TE images
    
    * Fix type annotations to work without optional dep
    
    * ah. I see it is I who is the fool
    
    * pre-commit fixes
    
    * pycharm lint, mostly
    
    * more lint...

---
+++ cle/backends/te.py
@@ -0,0 +1,108 @@
+import struct
+from collections import namedtuple
+
+import archinfo
+
+from . import Backend, Section, register_backend
+
+HEADER = struct.Struct("<HHBBHIIQIIII")
+SECTION_HEADER = struct.Struct("<8sIIIIIIHHI")
+HeaderType = namedtuple(
+    "HeaderType",
+    (
+        "signature",
+        "machine",
+        "number_of_sections",
+        "subsystem",
+        "stripped_size",
+        "address_of_entry_point",
+        "base_of_code",
+        "image_base",
+        "data_directory_0_virtual_address",
+        "data_directory_0_size",
+        "data_directory_1_virtual_address",
+        "data_directory_1_size",
+    ),
+)
+SectionHeaderType = namedtuple(
+    "SectionHeaderType",
+    (
+        "section_name",
+        "physical_address_virtual_size",
+        "virtual_address",
+        "size_of_raw_data",
+        "pointer_to_raw_data",
+        "pointer_to_relocations",
+        "pointer_to_line_numbers",
+        "number_of_relocations",
+        "number_of_line_numbers",
+        "characteristics",
+    ),
+)
+
+ARCH_MAPPING = {
+    0x014C: "i386",
+    0x0200: "IPF",
+    0x0EBC: "EBC",
+    0x8664: "X64",
+    0x01C0: "ARM Cortex-M",
+    0x01C2: "ARM",
+}
+
+
+class TE(Backend):
+    """
+    A "Terse Executable" format image, commonly used as part of UEFI firmware drivers.
+    """
+
+    is_default = True
+
+    @classmethod
+    def is_compatible(cls, stream):
+        stream.seek(0)
+        return stream.read(2) == b"VZ"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._binary_stream.seek(0)
+        self.header = HeaderType(*HEADER.unpack(self._binary_stream.read(HEADER.size)))
+        self.section_headers = [
+            SectionHeaderType(*SECTION_HEADER.unpack(self._binary_stream.read(SECTION_HEADER.size)))
+            for _ in range(self.header.number_of_sections)
+        ]
+
+        self.set_arch(archinfo.arch_from_id(ARCH_MAPPING[self.header.machine]))
+
+        offset_offset = self.header.stripped_size - HEADER.size
+        self.linked_base = self.mapped_base = self.header.image_base + offset_offset
+
+        has_relocs = False
+        for section_header in self.section_headers:
+            region = Section(
+                section_header.section_name.rstrip(b"\0").decode(),
+                section_header.pointer_to_raw_data,
+                section_header.virtual_address + self.linked_base,
+                section_header.physical_address_virtual_size,
+            )
+            self._sections.append(region)
+
+            if section_header.characteristics & 0x02000000 != 0 or section_header.physical_address_virtual_size == 0:
+                # discard or no data
+                continue
+            self._binary_stream.seek(section_header.pointer_to_raw_data - offset_offset)
+            data = self._binary_stream.read(section_header.size_of_raw_data)
+            assert len(data) == section_header.size_of_raw_data
+            if section_header.size_of_raw_data < section_header.physical_address_virtual_size:
+                data = data.ljust(section_header.physical_address_virtual_size - section_header.size_of_raw_data, b"\0")
+            self.memory.add_backer(section_header.virtual_address, data)
+
+            if section_header.number_of_relocations != 0:
+                has_relocs = True
+
+        self._segments = self._sections
+
+        self.pic = has_relocs
+
+
+register_backend("te", TE)
---
+++ cle/backends/uefi_firmware.py
@@ -0,0 +1,201 @@
+import io
+import logging
+import mmap
+from dataclasses import dataclass
+from functools import singledispatchmethod
+from typing import Dict, Optional
+from uuid import UUID
+
+import archinfo
+
+from cle.errors import CLEError, CLEUnknownFormatError
+
+from . import Backend, register_backend
+from .pe import PE
+from .te import TE
+
+try:
+    import uefi_firmware
+except ImportError:
+    uefi_firmware = None
+
+log = logging.getLogger(__name__)
+
+
+class UefiDriverLoadError(Exception):
+    """
+    This error is raised (and caught internally) if the data contained in the UEFI entity tree doesn't make sense.
+    """
+
+
+class UefiFirmware(Backend):
+    """
+    A UEFI firmware blob loader. Support is provided by the ``uefi_firmware`` package.
+    """
+
+    is_default = True
+
+    @classmethod
+    def _to_bytes(cls, fileobj: io.IOBase):
+        try:
+            fileno = fileobj.fileno()
+        except io.UnsupportedOperation:
+            pass
+        else:
+            return mmap.mmap(fileno, 0, access=mmap.ACCESS_READ)
+
+        if isinstance(fileobj, io.BytesIO):
+            return fileobj.getbuffer()
+
+        # fuck it, we'll do it live
+        fileobj.seek(0)
+        return fileobj.read()
+
+    @classmethod
+    def is_compatible(cls, stream):
+        if uefi_firmware is None:
+            return False
+
+        buffer = cls._to_bytes(stream)
+        parser = uefi_firmware.AutoParser(buffer)
+        return parser.type() != "unknown"
+
+    def __init__(self, *args, **kwargs):
+        if uefi_firmware is None:
+            raise CLEError("run `pip install uefi_firmware==1.10` to load UEFI firmware")
+        super().__init__(*args, **kwargs)
+
+        # hack: we are using a loader internal method in a non-kosher way which will cause our children to be
+        # marked as the main binary if we are also the main binary
+        # work around this by setting ourself here:
+        if self.loader.main_object is None:
+            self.loader.main_object = self
+
+        self._drivers: Dict[UUID, "UefiModuleMixin"] = {}
+        self._drivers_pending: Dict[UUID, "UefiModulePending"] = {}
+
+        self.set_arch(archinfo.arch_from_id("x86_64"))  # TODO: ???
+
+        buffer = self._to_bytes(self._binary_stream)
+        parser = uefi_firmware.AutoParser(buffer)
+        firmware = parser.parse()
+        self._load(firmware)
+
+        while self._drivers_pending:
+            uuid, pending = self._drivers_pending.popitem()
+            try:
+                child = pending.build(self, uuid)
+            except UefiDriverLoadError as e:
+                log.warning("Failed to load %s: %s", uuid, e.args[0])
+            else:
+                self._drivers[uuid] = child
+                child.parent_object = self
+                self.child_objects.append(child)
+
+        if self.child_objects:
+            self.arch = self.child_objects[0].arch
+        else:
+            log.warning("Loaded empty static archive?")
+        self.has_memory = False
+        self.pic = True
+
+        # hack pt. 2
+        if self.loader.main_object is self:
+            self.loader.main_object = None
+
+    @singledispatchmethod
+    def _load(self, uefi_obj):  # pylint: disable=no-self-use
+        raise CLEUnknownFormatError(f"Can't load firmware object: {uefi_obj}")
+
+    if uefi_firmware is not None:
+
+        @_load.register
+        def _load_generic(self, uefi_obj: uefi_firmware.FirmwareObject):
+            for obj in uefi_obj.objects:
+                self._load(obj)
+
+        @_load.register
+        def _load_none(self, uefi_obj: None):
+            pass
+
+        @_load.register
+        def _load_firmwarefile(self, uefi_obj: uefi_firmware.uefi.FirmwareFile):
+            if uefi_obj.type == 7:  # driver
+                uuid = UUID(bytes=uefi_obj.guid)
+                self._drivers_pending[uuid] = UefiModulePending()
+            self._load_generic(uefi_obj)
+
+        @_load.register
+        def _load_firmwarefilesection(self, uefi_obj: uefi_firmware.uefi.FirmwareFileSystemSection):
+            pending = self._drivers_pending.get(UUID(bytes=uefi_obj.guid), None)
+            if pending is not None:
+                if uefi_obj.type == 16:  # pe32 image
+                    pending.pe_image = uefi_obj.content
+                elif uefi_obj.type == 18:  # te image
+                    pending.te_image = uefi_obj.content
+                elif uefi_obj.type == 21:  # user interface name
+                    pending.name = uefi_obj.content.decode("utf-16").strip("\0")
+            self._load_generic(uefi_obj)
+
+
+@dataclass
+class UefiModulePending:
+    """
+    A worklist entry for the UEFI firmware loader.
+    """
+
+    name: Optional[str] = None
+    pe_image: Optional[bytes] = None
+    te_image: Optional[bytes] = None
+    # version
+    # dependencies
+
+    def build(self, parent: UefiFirmware, guid: UUID) -> "UefiModuleMixin":
+        count = (self.pe_image is not None) + (self.te_image is not None)
+        if count > 1:
+            raise UefiDriverLoadError("Multiple image sections")
+        if self.pe_image is not None:
+            cls = UefiPE
+            data = self.pe_image
+        elif self.te_image is not None:
+            cls = UefiTE
+            data = self.te_image
+        else:
+            raise UefiDriverLoadError("Missing PE or TE image section")
+        return cls(None, io.BytesIO(data), is_main_bin=False, loader=parent.loader, name=self.name, guid=guid)
+
+
+class UefiModuleMixin(Backend):
+    """
+    A mixin to make other kinds of backends load as UEFI modules.
+    """
+
+    def __init__(self, *args, guid: UUID, name: Optional[str], **kwargs):
+        super().__init__(*args, **kwargs)
+        self.guid = guid
+        self.user_interface_name = name
+
+        if self.linked_base == 0:
+            self.pic = True
+
+    def __repr__(self):
+        return (
+            f"<{type(self).__name__} Object "
+            f'{self.guid}{f" {self.user_interface_name}" if self.user_interface_name else ""}, '
+            f"maps [{self.min_addr:#x}:{self.max_addr:#x}]>"
+        )
+
+
+class UefiPE(UefiModuleMixin, PE):
+    """
+    A PE file contained in a UEFI image.
+    """
+
+
+class UefiTE(UefiModuleMixin, TE):
+    """
+    A TE file contained in a UEFI image.
+    """
+
+
+register_backend("uefi", UefiFirmware)
