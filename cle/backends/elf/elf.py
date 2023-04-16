e1cc21f096c1f54f5f1a45b26f2d87ab203f5e74
Atipriya <batipriya@gmail.com>
2023-04-16 14:52:25 -0700

    sort dict

--- cle/backends/elf/elf.py
+++ cle/backends/elf/elf.py
@@ -14,7 +14,6 @@ from elftools.dwarf.die import DIE
 from elftools.dwarf.dwarf_expr import DWARFExprParser
 from elftools.dwarf.dwarfinfo import DWARFInfo
 from elftools.elf import dynamic, elffile, enums, sections
-from sortedcontainers import SortedDict
 
 from cle.address_translator import AT
 from cle.backends.backend import ExceptionHandling, FunctionHint, FunctionHintSource, register_backend
@@ -664,7 +663,8 @@ class ELF(MetaELF):
 
                 relocated_addr = AT.from_lva(line.state.address, self).to_mva()
                 self.addr_to_line[relocated_addr].add((filename, line.state.line))
-
+        self.addr_to_line = defaultdict(set, sorted(self.addr_to_line.items()))
+                
     @staticmethod
     def _load_low_high_pc_form_die(die: DIE):
         """
