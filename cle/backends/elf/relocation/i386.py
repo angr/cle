71ebaa2bdf97d6bb6e1b9771d3e5c89e582de680
Han Dai <github@daihan.me>
2024-01-13 13:18:25 -0500

    doc string

--- cle/backends/elf/relocation/i386.py
+++ cle/backends/elf/relocation/i386.py
@@ -1,4 +1,3 @@
-# pylint:disable=missing-class-docstring
 from .generic import (
     GenericAbsoluteAddendReloc,
     GenericCopyReloc,
@@ -16,48 +15,108 @@ arch = "X86"
 
 
 class R_386_32(GenericAbsoluteAddendReloc):
+    """
+    Value: 1
+    Field: word32
+    Calculation: S + A
+    """
     pass
 
 
 class R_386_PC32(GenericPCRelativeAddendReloc):
+    """
+    Value: 2
+    Field: word32
+    Calculation: S + A - P
+    """
     pass
 
 
 class R_386_COPY(GenericCopyReloc):
+    """
+    Value: 5
+    Field:
+    Calculation:
+    """
     pass
 
 
 class R_386_GLOB_DAT(GenericJumpslotReloc):
+    """
+    Value: 6
+    Field: word32
+    Calculation: S
+    """
     pass
 
 
 class R_386_JMP_SLOT(GenericJumpslotReloc):
+    """
+    Value: 7
+    Field: word32
+    Calculation: S
+    """
     pass
 
 
 class R_386_RELATIVE(GenericRelativeReloc):
+    """
+    Value: 8
+    Field: word32
+    Calculation: B + A
+    """
     pass
 
 
 class R_386_IRELATIVE(GenericIRelativeReloc):
+    """
+    Value: 42
+    Field: word32
+    Calculation: indirect (B + A)
+    """
     pass
 
 
 class R_386_TLS_DTPMOD32(GenericTLSModIdReloc):
+    """
+    Value: 35
+    Field: word32
+    Calculation: 
+    """
     pass
 
 
 class R_386_TLS_TPOFF(GenericTLSOffsetReloc):
+    """
+    Value: 14
+    Field: word32
+    Calculation: 
+    """
     pass
 
 
 class R_386_TLS_DTPOFF32(GenericTLSDoffsetReloc):
+    """
+    Value: 36
+    Field: word32
+    Calculation: 
+    """
     pass
 
 
 class R_386_PLT32(GenericPCRelativeAddendReloc):
+    """
+    Value: 4
+    Field: word32
+    Calculation: L + A - P
+    """
     pass
 
 
 class R_386_GOTPC(GenericPCRelativeAddendReloc, RelocGOTMixin):
+    """
+    Value: 10
+    Field: word32
+    Calculation: GOT + A - P
+    """
     pass
