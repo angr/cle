19245474799b6c3a810cf8e3abd71daa31b3d4aa
Caleb Helbling <chelbling@draper.com>
2025-07-09 15:03:18 -0400

    Added support for enumeration and subroutine DWARF types

--- cle/backends/elf/variable_type.py
+++ cle/backends/elf/variable_type.py
@@ -4,7 +4,6 @@ from enum import Enum
 
 from elftools.dwarf.die import DIE
 
-
 class VariableType:
     """
     Entry class for DW_TAG_xxx_type
@@ -40,6 +39,10 @@ class VariableType:
             return TypedefType.read_from_die(die, elf_object)
         elif die.tag == "DW_TAG_union_type":
             return UnionType.read_from_die(die, elf_object)
+        elif die.tag == "DW_TAG_enumeration_type":
+            return EnumerationType.read_from_die(die, elf_object)
+        elif die.tag == "DW_TAG_subroutine_type":
+            return SubroutineType.read_from_die(die, elf_object)
         return None
 
     @staticmethod
@@ -51,6 +54,8 @@ class VariableType:
             "DW_TAG_array_type",
             "DW_TAG_typedef",
             "DW_TAG_union_type",
+            "DW_TAG_enumeration_type",
+            "DW_TAG_subroutine_type"
         )
 
 
@@ -63,8 +68,10 @@ class PointerType(VariableType):
     :param referenced_offset:  type of the referenced as offset in the compilation_unit
     """
 
-    def __init__(self, byte_size: int, elf_object, referenced_offset: int):
-        super().__init__("pointer", byte_size, elf_object)
+    def __init__(self, name: str | None, byte_size: int, elf_object, referenced_offset: int):
+        if name is None:
+            name = "pointer"
+        super().__init__(name, byte_size, elf_object)
         self._referenced_offset = referenced_offset
 
     @classmethod
@@ -74,6 +81,9 @@ class PointerType(VariableType):
         byte_size or type attribute.
         """
         byte_size_attr = die.attributes.get("DW_AT_byte_size", None)
+
+        name_attr = die.attributes.get("DW_AT_name", None)
+        name = None if name_attr is None else name_attr.value.decode()
 
         if byte_size_attr is not None:
             byte_size = byte_size_attr.value
@@ -88,7 +98,7 @@ class PointerType(VariableType):
         else:
             referenced_offset = dw_at_type.value + die.cu.cu_offset
 
-        return cls(byte_size, elf_object, referenced_offset)
+        return cls(name, byte_size, elf_object, referenced_offset)
 
     @property
     def referenced_type(self):
@@ -352,3 +362,99 @@ class TypedefType(VariableType):
         if self._type_offset in type_list.keys():
             return type_list[self._type_offset]
         return None
+
+class EnumeratorValue:
+    def __init__(self, name: str, const_value):
+        self.name = name
+        self.const_value = const_value
+
+    @classmethod
+    def read_from_die(cls, die: DIE, elf_object):
+        name_attr = die.attributes.get("DW_AT_name", None)
+        name = None if name_attr is None else name_attr.value.decode()
+
+        const_value_attr = die.attributes.get("DW_AT_const_value", None)
+        const_value = None if const_value_attr is None else const_value_attr.value
+
+        return cls(name, const_value)
+
+class EnumerationType(VariableType):
+    def __init__(self, name: str, byte_size: int, elf_object, enumerator_values: list[EnumeratorValue]):
+        super().__init__(name, byte_size, elf_object)
+        self.enumerator_values = enumerator_values
+
+    def __len__(self):
+        return len(self.enumerator_values)
+
+    def __iter__(self):
+        return iter(self.enumerator_values)
+
+    @classmethod
+    def read_from_die(cls, die: DIE, elf_object):
+        """
+        read an entry of DW_TAG_enumeration_type.
+        """
+
+        dw_byte_size = die.attributes.get("DW_AT_byte_size", None)
+        byte_size = dw_byte_size.value if dw_byte_size is not None else None
+
+        name_attr = die.attributes.get("DW_AT_name", None)
+        name = None if name_attr is None else name_attr.value.decode()
+
+        enumerators = []
+        for child in die.iter_children():
+            match child.tag:
+                case "DW_TAG_enumerator":
+                    enumerators.append(EnumeratorValue.read_from_die(child, elf_object))
+
+        return cls(name, byte_size, elf_object, enumerators)
+
+class SubroutineType(VariableType):
+    def __init__(self, name: str, byte_size: int, elf_object, type_offset: int | None, parameter_offsets):
+        super().__init__(name, byte_size, elf_object)
+        self._type_offset = type_offset
+        self._parameter_offsets = parameter_offsets
+
+    @property
+    def type(self):
+        """
+        The return type of the subroutine, or None if the subroutine returns no value
+        """
+        if self._type_offset is None:
+            return None
+        else:
+            return self._elf_object.type_list[self._type_offset]
+
+    @property
+    def parameters(self):
+        """
+        Iterates over the parameters of the subroutine
+        """
+        type_list = self._elf_object.type_list
+        for offset in self._parameter_offsets:
+            yield type_list[offset]
+
+    @classmethod
+    def read_from_die(cls, die: DIE, elf_object):
+        """
+        read an entry of DW_TAG_subroutine_type
+        """
+
+        dw_byte_size = die.attributes.get("DW_AT_byte_size", None)
+        byte_size = dw_byte_size.value if dw_byte_size is not None else None
+
+        name_attr = die.attributes.get("DW_AT_name", None)
+        name = None if name_attr is None else name_attr.value.decode()
+
+        dw_at_type = die.attributes.get("DW_AT_type", None)
+        type_offset = None if dw_at_type is None else dw_at_type.value + die.cu.cu_offset
+
+        parameter_offsets: list[int] = []
+        for child in die.iter_children():
+            match child.tag:
+                case "DW_TAG_formal_parameter":
+                    param_type_attr = child.attributes.get("DW_AT_type", None)
+                    param_type_offset = None if param_type_attr is None else param_type_attr.value + die.cu.cu_offset
+                    parameter_offsets.append(param_type_offset)
+
+        return cls(name, byte_size, elf_object, type_offset, parameter_offsets)
