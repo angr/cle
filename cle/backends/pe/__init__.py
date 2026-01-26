d95f5aa8a3ac7a005f23eb4988e477309dbc4ca7
Fish <ltfish@users.noreply.github.com>
2026-01-26 14:34:39 -0500

    Windows PE debug symbol discovery and download support (#644)
    * Preliminary implementation of PE symbol downloading support.
    
    * Add callbacks.
    
    * Tweaks and fixes.
    
    * Type check.
    
    * Lint and type check.

--- cle/backends/pe/__init__.py
+++ cle/backends/pe/__init__.py
@@ -2,8 +2,14 @@ from __future__ import annotations
 
 from .pe import PE
 from .stubs import PEStubs
+from .symbolserver import DownloadCancelledError, PDBInfo, SymbolPathParser, SymbolResolver, SymbolServerClient
 
 __all__ = [
+    "DownloadCancelledError",
     "PE",
     "PEStubs",
+    "PDBInfo",
+    "SymbolPathParser",
+    "SymbolResolver",
+    "SymbolServerClient",
 ]
