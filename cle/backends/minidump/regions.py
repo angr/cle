b04c196b99516dfbe43560200dbcf9142e0d4931
r1ngz3ro <madlad4567@gmail.com>
2026-05-07 19:38:26 +0100

    added memory protections to segments in the  minidump backend

---
+++ cle/backends/minidump/regions.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+from cle.backends.region import Section
+from minidump.streams import MemoryInfoListStream
+
+
+AP = MemoryInfoListStream.AllocationProtect
+
+
+class DumpSection(Section):
+    """
+    Represents a mapped memory section in a dump.
+    """
+
+    def __init__(
+        self,
+        module,
+        segment,
+        protect,
+        vaddr=None,
+        size=None,
+    ):
+        super().__init__(
+            module.name,
+            segment.start_file_address + (vaddr - segment.start_virtual_address) if vaddr else segment.start_file_address,
+            vaddr or module.baseaddress,
+            size or module.size
+        )
+        self.protect = protect
+
+    @property
+    def is_readable(self) -> bool:
+        readable = (
+            AP.PAGE_READONLY.value
+            | AP.PAGE_READWRITE.value
+            | AP.PAGE_WRITECOPY.value
+            | AP.PAGE_EXECUTE_READ.value
+            | AP.PAGE_EXECUTE_READWRITE.value
+            | AP.PAGE_EXECUTE_WRITECOPY.value
+        )
+        return bool(self.protect.value & readable)
+
+    @property
+    def is_writable(self) -> bool:
+        writable = (
+            AP.PAGE_READWRITE.value
+            | AP.PAGE_EXECUTE_READWRITE.value
+        )
+        return bool(self.protect.value & writable)
+
+    @property
+    def is_executable(self) -> bool:
+        executable = (
+            AP.PAGE_EXECUTE.value
+            | AP.PAGE_EXECUTE_READ.value
+            | AP.PAGE_EXECUTE_READWRITE.value
+            | AP.PAGE_EXECUTE_WRITECOPY.value
+        )
+        return bool(self.protect.value & executable)
+
